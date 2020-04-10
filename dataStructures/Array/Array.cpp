f5f48019f8c2f875210700e2c6b0af48a0a77314
Devamakesh <47493737+devamakeshr@users.noreply.github.com>
2020-04-10 12:46:48 +0530

    add java files

--- dataStructures/Array/Array.cpp
+++ dataStructures/Array/Array.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+class Array
+{
+public:
+    int arr[] = {null};
+    Array(int size);
+    int insertArrayElement(int value, int location)
+    {
+
+        if (arr[location] == INT_MIN)
+        {
+            arr[location] = value;
+            cout << "item " << value << " is successfully add";
+        }
+        else
+        {
+            cout << "we value in the location";
+        }
+        return 0;
+    }
+};
+int Array::Array(int size)
+{
+    int arr[size];
+    for (int i = 0; i > size; i++)
+    {
+        arr[i] = INT_MIN;
+    }
+    return 0;
+}
+
 int main()
 {
-    cout << "hello";
+    Array arr(8);
+    arr.insertArrayElement(5, 0);
 }
